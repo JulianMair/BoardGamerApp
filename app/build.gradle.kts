f573435952bce0cb8d02274556544b94aba47bf5
Kalaschnimoe <49235968+Kalaschnimoe@users.noreply.github.com>
2025-12-03 20:39:21 +0100

    grundaufbau importiert und lauffähige app erstellt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
+val room_version = "2.6.1"
+
 plugins {
     id("com.android.application")
 }
-
 android {
     namespace = "de.iu.boardgame"
     compileSdk = 34
@@ -33,39 +34,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    //----------------- Room dependencies ---------------------------------
-    val room_version = "2.8.4"
-
     implementation("androidx.room:room-runtime:$room_version")
-
-    // If this project only uses Java source, use the Java annotationProcessor
-    // No additional plugins are necessary
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
-    // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:$room_version")
 
-    // optional - RxJava2 support for Room
-    implementation("androidx.room:room-rxjava2:$room_version")
-
-    // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$room_version")
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation("androidx.room:room-guava:$room_version")
-
-    // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$room_version")
-
-    // optional - Paging 3 Integration
-    implementation("androidx.room:room-paging:$room_version")
-    //------------------End Room --------------------------------------------------
 }
