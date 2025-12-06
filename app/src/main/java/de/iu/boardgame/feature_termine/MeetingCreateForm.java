6a668c40345fdf5ab1d4f889c911a1ce995072d1
chriscodecc <eppidemie@gmail.com>
2025-12-06 13:48:41 +0100

    - chain date and time picker

--- app/src/main/java/de/iu/boardgame/feature_termine/MeetingCreateForm.java
+++ app/src/main/java/de/iu/boardgame/feature_termine/MeetingCreateForm.java
@@ -1,10 +1,12 @@
 package de.iu.boardgame.feature_termine;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
@@ -14,46 +16,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
 import de.iu.boardgame.R;
 import de.iu.boardgame.feature_termine.helpers.DatePickerFragment;
 import de.iu.boardgame.feature_termine.helpers.TimePickerFragment;
 
 public class MeetingCreateForm extends AppCompatActivity
-            implements TimePickerDialog.OnTimeSetListener {
+            implements DatePickerDialog.OnDateSetListener,
+                       TimePickerDialog.OnTimeSetListener {
 
-        @Override
+    DialogFragment datepicker;
+    DialogFragment timepicker;
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             EdgeToEdge.enable(this);
             setContentView(R.layout.activity_meeting_create_form);
-            Button pickTimeButton = findViewById(R.id.pickTime);
-            Button pickDateButton = findViewById(R.id.pickDate);
+
+            Button btndate = findViewById(R.id.btnSelect);
+
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
                 Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
                 return insets;
             });
 
-            pickTimeButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    DialogFragment timepicker = new TimePickerFragment();
-                    timepicker.show(getSupportFragmentManager(), "time picker");
-                }
-            });
-
-            pickDateButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    DialogFragment datepicker = new DatePickerFragment();
-                    datepicker.show(getSupportFragmentManager(), "date picker");
-                }
+            // Öffnen des Datums auswahl Dialogs
+            btndate.setOnClickListener(view -> {
+                DatePickerFragment dateFragment = new DatePickerFragment();
+                dateFragment.show(getSupportFragmentManager(), "datePicker");
             });
         }
 
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            String selectedTime = String.format("%02d:%02d", hourOfDay, minute);
-            Log.d("MeetingForm", "Zeit erfolgreich ausgewählt: " + selectedTime);
-        }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Log.d("THIS IS A TE>XT", String.valueOf(year));
+        create_Time_Dialog();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+
+    public void create_Time_Dialog() {
+        TimePickerFragment timeFragment = new TimePickerFragment();
+        timeFragment.show(getSupportFragmentManager(), "timePicker");
+    }
 }
--- app/src/main/java/de/iu/boardgame/feature_termine/helpers/DatePickerFragment.java
+++ app/src/main/java/de/iu/boardgame/feature_termine/helpers/DatePickerFragment.java
@@ -2,19 +2,23 @@ package de.iu.boardgame.feature_termine.helpers;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment {
+    int selectedYear;
+    int selecteDay;
+    int selectedMonth;
+
     @NonNull
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         // Use the current date as the default date in the picker.
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
@@ -22,11 +26,42 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it.
-        return new DatePickerDialog(requireContext(),this, year, month, day);
+        return new DatePickerDialog(
+                getActivity(),
+                (DatePickerDialog.OnDateSetListener) getActivity(),
+                year,
+                month,
+                day);
+    }
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        this.selecteDay = day;
+        this.selectedMonth = month;
+        this.selectedYear = year;
     }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
+    public void setDay(int day){
+        this.selecteDay = day;
     }
+
+    public void setMonth(int month){
+        this.selectedMonth = month;
+    }
+
+    public void setYear(int year){
+        this.selectedYear = year;
+    }
+
+    public int getDay () {
+        return selecteDay;
+    }
+
+    public int getMonth() {
+        return selectedMonth;
+    }
+
+    public int getYear(){
+        return selectedYear;
+    }
+
+
 }
